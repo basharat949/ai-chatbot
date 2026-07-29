406ffa6608b767a759ad124d9bf60dae62a99845
basharat949 <hafiz.m.basharat@gmail.com>
2026-07-30 02:04:32 +0500

    sort the files

--- alembic/versions/8d0db4c2405d_create_users_table.py
+++ alembic/versions/8d0db4c2405d_create_users_table.py
@@ -7,9 +7,9 @@ Create Date: 2026-07-29 18:18:48.528936
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '8d0db4c2405d'
